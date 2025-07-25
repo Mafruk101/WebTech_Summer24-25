5ffa89794533327f7c8f42e6bc7fd12b9bab23b7
Mafruk101 <mafrukhosen1010@gmail.com>
2025-07-25 19:48:45 +0600

    hsdjhfja

--- Mid/labTask2.php
+++ Mid/labTask2.php
@@ -1,11 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>hello world, this is git pull push testing </h2>
-</body>
-</html>
+pull push testing 
