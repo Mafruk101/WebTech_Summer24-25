5dbd054ef714fa19c492c8e8b9481a10d6b62d74
Mafruk101 <mafrukhosen1010@gmail.com>
2025-07-25 17:32:10 +0600

    lab2

---
+++ labTask1.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+    <title>Registration form</title>
+    <body>
+        <center>
+            <h1 style="color: blue;">AIUB </h1>
+            <h2 style="color: blue;">Registration Form </h2>
+        </center>
+        <h3>Complete the Registration</h3>
+        <table>
+            <tr>
+                <td>Full Name:</td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>Email:</td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>Password:</td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>Gender:</td>
+                <td><input type="radio" name="des">Male 
+                <input type="radio" name="des">Female </td>
+            </tr>
+            <tr>
+                <td>Language Known:</td>
+                <td><input type="checkbox">English 
+                <input type="checkbox" >Bangla 
+                <input type="checkbox" >Hindi </td>
+            </tr>
+            <tr>
+                <td>Country:</td>
+                <td><select name="Country:"> 
+                    <option value="">--select--</option>
+                    <option value="">Bangladesh</option>
+                    <option value="">Australia</option></td>
+                </select>
+                
+            </tr>
+
+            <tr>
+                <td>Date of Birth:</td>
+                <td><input type="date"></td>
+            </tr>
+            <tr>
+                <td>Upload Photo:</td>
+                <td><input type="file"></td>
+            </tr>
+            <tr>
+                <td>Comments:</td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>        </td>
+                <td><input type="button" value ="Register"></td>
+            </tr>
+        </table>
+</body>
+</html>
+                
